81526b64cee315e707be3577a531b769ef9fdf6a
Karpova Anna <anyakarpova9494@gmail.com>
2024-03-12 18:44:30 +0300

    проект до проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+const createCard = (card) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const img = cardElement.querySelector(".card__image");
+  img.src = card.link;
+  img.alt = card.name;
+
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
+  cardsContainer.append(cardElement);
+};
+
 // @todo: Функция удаления карточки
 
+const deleteCard = (card) => {
+  card.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  createCard(item);
+});
